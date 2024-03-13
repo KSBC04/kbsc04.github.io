8b5992308593641ac65f33d35ca481a8f13a61b1
Kayllane <kayllanecardoso@icloud..com>
2024-03-12 23:28:54 -0300

    paginas

--- 18.03/js/app.js
+++ 18.03/js/app.js
@@ -1,11 +1,110 @@
-let btn1 = document.getElementById("btn1");
-btn1 = function(){
+//p 1
+let btn1 = function(){
+        window.location.href = "./2.html"
+}
+//p 2
+let btn2 = function(){
+        window.location.href = "./index.html"
+}
+let btn3 = function(){
+        window.location.href = "./3.html"
+}
+//p 3
+let btn4 = function(){
+        window.location.href = "./2.html"
+}
+let btn5 = function(){
+        window.location.href = "./4.html"
+}
+//p 4
+let btn6 = function(){
     window.location.href = "./3.html"
 }
-function abreImg1 (id){
-    document.getElementById("select").innerHTML = "<h2>Festa Junina</h2> <p>Pode parecer estranho eu começar a linha cronológica do nosso relacionamento, nós já nos conhecíamos há muito tempo, começamos há conversar realmente quase um mês depois dessa festa, mas juro que faz sentido. Quando começaram os ensaios para a festa junina, eu comecei a ficar mais próximo a você, não foi por causa da festa, mas aconteceu na mesma época. Uma vez você me pediu para ensaiar com você, porque a Nina não poderia e você ainda não tinha ensaiado, eu aceitei e ensaiamos juntos naquele dia, a Duda ficou reclamando porque ela tinha quase que implorado para eu dançar com ela, mas você só teve que pedir uma vez e eu fui. No dia da festa, você e a Nina tinham brigado e você estava meio triste e não queria dançar com ela, de alguma forma que eu não me lembro muito bem, eu quase dancei com você naquele dia, mas tive que ir embora mais cedo e não pude. Fiquei triste por isso, eu realmente queria ter dançado com você naquele dia, porque tinha me divertido dançando com você no ensaio. </br> Depois daquela festa, alguém perguntou para mim de nós como um possível casal pela primeira vez, você com certeza já sabe quem foi: Isabela Frotté. Eu estava na biblioteca com ela e a Sara, elas falaram sobre os namorados e como elas achavam legal as duas terem conhecido eles no cefet e comentaram dos garotos que tinham gostado antes, então a Isabela falou: “E você Kayllane, está gostando de alguém?”, eu disse que não que era muito desliga sobre essa coisas, não estava com o menor interesse em gostar de alguém, e aí ela disse “Tem certeza? E você e o Pablo? Senti que tinha algo entre vocês na festa junina” e eu respondi “Ah, o Pablo? Não, nós somos só amigos mesmo, não rola nada”. Apesar da minha resposta, aquilo ficou na minha cabeça, a Frotté é muito boa em ler pequenos sinais."
-
+let btn7 = function(){
+    window.location.href = "./5.html"
 }
+let btn8 = function(){
+    window.location.href = "./4.html"
+}
+let btn9 = function(){
+    window.location.href = "./6.html"
+}
+let btn10 = function(){
+    window.location.href = "./5.html"
+}
+let btn11 = function(){
+    window.location.href = "./7.html"
+}
+let btn12 = function(){
+    window.location.href = "./6.html"
+}
+let btn13 = function(){
+    window.location.href = "./8.html"
+}
+let btn14 = function(){
+    window.location.href = "./7.html"
+}
+let btn15 = function(){
+    window.location.href = "./9.html"
+}
+let btn16 = function(){
+    window.location.href = "./8.html"
+}
+let btn17 = function(){
+    window.location.href = "./10.html"
+}
+let btn18 = function(){
+    window.location.href = "./9.html"
+}
+let btn19 = function(){
+    window.location.href = "./11.html"
+}
+let btn20 = function(){
+    window.location.href = "./10.html"
+}
+let btn21 = function(){
+    window.location.href = "./12.html"
+}
+let btn22 = function(){
+    window.location.href = "./11.html"
+}
+let btn23 = function(){
+    window.location.href = "./13.html"
+}
+let btn24 = function(){
+    window.location.href = "./12.html"
+}
+let btn25 = function(){
+    window.location.href = "./14.html"
+}
+let btn26 = function(){
+    window.location.href = "./13.html"
+}
+let btn27 = function(){
+    window.location.href = "./15.html"
+}
+let btn28 = function(){
+    window.location.href = "./14.html"
+}
+let btn29 = function(){
+    window.location.href = "./16.html"
+}
+let btn30 = function(){
+    window.location.href = "./15.html"
+}
+
+function abreImg1(){
+    document.getElementById("select").innerHTML = "<h2>Festa Junina</h2> <h4>Pode parecer estranho eu começar a linha cronológica do nosso relacionamento, nós já nos conhecíamos há muito tempo, começamos há conversar realmente quase um mês depois dessa festa, mas juro que faz sentido. Quando começaram os ensaios para a festa junina, eu comecei a ficar mais próximo a você, não foi por causa da festa, mas aconteceu na mesma época. Uma vez você me pediu para ensaiar com você, porque a Nina não poderia e você ainda não tinha ensaiado, eu aceitei e ensaiamos juntos naquele dia, a Duda ficou reclamando porque ela tinha quase que implorado para eu dançar com ela, mas você só teve que pedir uma vez e eu fui. No dia da festa, você e a Nina tinham brigado e você estava meio triste e não queria dançar com ela, de alguma forma que eu não me lembro muito bem, eu quase dancei com você naquele dia, mas tive que ir embora mais cedo e não pude. Fiquei triste por isso, eu realmente queria ter dançado com você naquele dia, porque tinha me divertido dançando com você no ensaio. </br> Depois daquela festa, alguém perguntou para mim de nós como um possível casal pela primeira vez, você com certeza já sabe quem foi: Isabela Frotté. Eu estava na biblioteca com ela e a Sara, elas falaram sobre os namorados e como elas achavam legal as duas terem conhecido eles no cefet e comentaram dos garotos que tinham gostado antes, então a Isabela falou: “E você Kayllane, está gostando de alguém?”, eu disse que não que era muito desliga sobre essa coisas, não estava com o menor interesse em gostar de alguém, e aí ela disse “Tem certeza? E você e o Pablo? Senti que tinha algo entre vocês na festa junina” e eu respondi “Ah, o Pablo? Não, nós somos só amigos mesmo, não rola nada”. Apesar da minha resposta, aquilo ficou na minha cabeça, a Frotté é muito boa em ler pequenos sinais.</h4>"
+}
+
+function abreImg2(){
+    document.getElementById("select").innerHTML = "<h2>Primeiros meses</h2> <h4>Você me mandou esse gif em setembro, aquela altura, falar com você e te ter por perto já eram coisas comuns para mim, eu já me sentia confortável com você. Apesar de ainda não ter noção verdadeira da pessoa incrível que estava ao meu lado, te ter por perto e falar diariamente com você já haviam se tornado coisas fundamentais para mim, qualquer pessoa que via de fora já estava dizendo que tínhamos algo, mas eu não me importava nem um pouco com o que os outros diziam, eu só queria estar perto de você e poder ser sua amiga. Eu nunca fui muito boa em me abrir para as pessoas, muito menos em iniciar assuntos e manter contato com alguém, mas com você foi diferente, sempre foi fácil falar com você, eu me sentia segura, ria muito, estava realmente feliz, não posso dizer que não exigiu esforço iniciar nossa relação, mas posso dizer que com certeza ele foi mínimo e o prazer que eu sentia falando com você era muito maior. Eu já me sentia atraída por você, sei que você já queria ficar comigo, mas acho que esses meses iniciais onde fomos apenas amigos foram fundamentais para o nossonamoro ser como é, podemos falar sobre tudo, somos verdadeiramente íntimos um do outro desde o início e confiamos um no outro, acho que só tivemos essas coisas desde o primeiro mês porque já éramos melhores amigos antes de sermos namorados.<h4>"
+}
+
+function abreImg3(){
+    document.getElementById("select").innerHTML = "<h2>A primeira carta</h2> <h4><h4>"
+}
+
 function abreVideo (id){
     document.getElementById("select").innerHTML = "<video src=" +document.getElementById(id).src+ ">"
 }
