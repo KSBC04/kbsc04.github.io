654e3a46847bffb7c0ee7bb91edb233328c6587d
Kayllane <kayllanecardoso@icloud..com>
2024-03-13 16:20:03 -0300

    textos

--- 18.03/js/app.js
+++ 18.03/js/app.js
@@ -23,72 +23,84 @@ let btn6 = function(){
 let btn7 = function(){
     window.location.href = "./5.html"
 }
+//p 5
 let btn8 = function(){
     window.location.href = "./4.html"
 }
 let btn9 = function(){
     window.location.href = "./6.html"
 }
+//p 6
 let btn10 = function(){
     window.location.href = "./5.html"
 }
 let btn11 = function(){
     window.location.href = "./7.html"
 }
+//p 7
 let btn12 = function(){
     window.location.href = "./6.html"
 }
 let btn13 = function(){
     window.location.href = "./8.html"
 }
+//p 8
 let btn14 = function(){
     window.location.href = "./7.html"
 }
 let btn15 = function(){
     window.location.href = "./9.html"
 }
+//p 9
 let btn16 = function(){
     window.location.href = "./8.html"
 }
 let btn17 = function(){
     window.location.href = "./10.html"
 }
+//p 10
 let btn18 = function(){
     window.location.href = "./9.html"
 }
 let btn19 = function(){
     window.location.href = "./11.html"
 }
+//p 11
 let btn20 = function(){
     window.location.href = "./10.html"
 }
 let btn21 = function(){
     window.location.href = "./12.html"
 }
+//p 12
 let btn22 = function(){
     window.location.href = "./11.html"
 }
 let btn23 = function(){
     window.location.href = "./13.html"
 }
+//p 13
 let btn24 = function(){
     window.location.href = "./12.html"
 }
 let btn25 = function(){
     window.location.href = "./14.html"
 }
+//p 14
 let btn26 = function(){
     window.location.href = "./13.html"
 }
 let btn27 = function(){
     window.location.href = "./15.html"
 }
+//p 15
 let btn28 = function(){
     window.location.href = "./14.html"
 }
 let btn29 = function(){
     window.location.href = "./16.html"
 }
+//p 16
 let btn30 = function(){
     window.location.href = "./15.html"
 }
@@ -102,9 +114,9 @@ function abreImg2(){
 }
 
 function abreImg3(){
-    document.getElementById("select").innerHTML = "<h2>A primeira carta</h2> <h4><h4>"
+    document.getElementById("select").innerHTML = "<h2>A primeira carta</h2> <h4>Acho que esse título pode ser uma romantização do que esse print realmente representa, talvez o título mais correto seria 'o fora' ou até mesmo 'meu maior erro'. Até hoje não sei dizer muito bem o que passou na minha cabeça quando soube do grupo, possivelmente porque várias coisas passaram ao mesmo tempo na minha cabeça. Assim que eu soube do grupo, me questionei se nossa amiazade era uma mentira, se você só tinha se aproximado de mim para ficar comigo; depois pensei como nada disso combinaria com você. Em um segundo momento, a Duda e a Dhiovana começaram a falar várias coisas para mim, reforçando tudo ruim que eu havia pensado e dizendo como eu deveria me afastar de você,porque não existia amizade e nunca havia existido, eu não consegui aceitar isso, eu fiz uma enorme cena a partir desse momento, meu mundo parecia ter caído, eu fiz um verdadeiro drama adolescente de filme. Chorei, gritei, surtei, até as meninas pararem e realmente sentarem para conversar comigo, me perguntarem qual o motivo de eu não querer perder sua amizade e qual o motivo de eu gostar tando de ficar com você. Eu respondi que você me deixava a vontade para falar sobre qualquer coisa, sobre como eu podia ser boba, mas também ter conversas mais profundas e sérias, que eu não conseguia ter com mais ninguém, falei sobre como você era inteligente, mas não se achava por isso, como você sabia levar qualquer conversa com qualquer pessoa e isso sempre me deixava confortável, e sobre o fato de você ter vários gostos e hobbies que eu admirava, como o piano por exemplo. Acho que qualquer outra pessoa que ouvisse isso saberia que eu estava apaixonada por você, exceto eu, para mim eu só estava muito abalada com a ideia de perder meu melhor amigo. Você já sabe todo o resto da história preticamente, o assusnto não saia da minha cabeça, quanto mais eu pensava sobre, mais eu percebia que eu podia estar apaixonada por você (e de fato eu estava), mas eu escrevi a carta apesar disso, falei coisas que não me arrependo, mas tem uma que escrevi em todos os rascunhos que me arrependo de não ter dito para você: 'Apasar de achar que nenhum de nós dois está pronto para lidar com isso como devemos, quero que saiba que você é a pessoa mais apaixonante que eu já conheci e, se fosse para eu escolher qualquer pessoa no mundo para eu me relacionar, seria você. Mas não posso me colocar nessa situação de talvez perder meu melhor amigo por isso.'. Mesmo sentindo que se eu tivesse tido isso o tempo do nosso relacionamento teria sido outro e talvez e tivessimos ficado juntos muito antes, eu gosto de como tudo aconteu. <br> Para concluir, fico muito feliz de você ter cumprido a promessa que me fez nessa mensagem e que agora sejamos o casal que somos.<h4>"
 }
 
-function abreVideo (id){
-    document.getElementById("select").innerHTML = "<video src=" +document.getElementById(id).src+ ">"
+function abreImg4(){
+    document.getElementById("select").innerHTML = "<h2>Cefetalent</h2> <h4>Eu te entregui a carta em outubro eu acho, depois disso teve a copa, as meninas me excluindo, você me contando sobre a Luísa, eu tinha certeza que tinha te perdido, eu nem mesmo poderia reclamar se você tivesse desistindo por mim depois da carta e da forma que eu agi quando soube que você estava gostando de mim. Mas aí teve o cefetalent, o dia que finalmente eu iria te ver de gogoboy e tocando piano, eu fiquei realmente animada para isso. Eu estava dando chocotones para naquela época do ano e já tinha falado com a Mylenna como você iria reagir quando te entregasse o seu, eu acertei em cheio. Acho que aquele dia foi a primeira vez que senti muita vontade de te beijar, a cada vez que eu olhava nos seus olhos parecia que o mundo todo parava e só nós dois existiamos, quando você estava tocando a música que compôs, meus olhos estavam vidrados em você e eu não conseguia desviar nem por um momento e tudo que eu queria era que você olhasse pára mimcom a mesma intensidade. Infelizmente, foi mais um dia onde nada aconteceu, mas eu não pude mais negar, eu estava apaixonada por você, só me restava saber se você também estava.<h4>"
 }
